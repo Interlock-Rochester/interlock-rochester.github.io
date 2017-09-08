bb4344f0d356b07a120cdb7eb0ce7958eda936f0
Josh Watts <josh+github@sroz.net>
2017-09-07 22:26:00 -0400

    settings: Default to relative links
    This makes debugging easier by default

--- pelicanconf.py
+++ pelicanconf.py
@@ -47,7 +47,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 STATIC_PATHS = ['images', 'wp-uploads']
 
